f4020d9c44d9a9aa07f31c0aa82c5dfc112590bf
Mustafa Shikora <mshik3@gmail.com>
2025-09-03 21:51:51 -0500

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    __WORKER_URL__: JSON.stringify(
+      process.env.VITE_WORKER_URL ||
+        "https://super-tic-tac-toe-worker.mshik3.workers.dev"
+    ),
+  },
+});
